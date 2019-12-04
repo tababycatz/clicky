acc42b812e1b5b3f40e1e0a89eeba3802b1e5ffb
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-12-04 13:25:27 -0500

    final updates

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => (
+  <div className="banner text-center d-flex align-items-center justify-content-center">
+    <p className="m-0">Click on a picture, but don't click it twice!</p>
+  </div>
+);
+
+export default Banner;
---
+++ src/images.js
@@ -0,0 +1,21 @@
+import bob from './images/bob.png';
+import gayle from './images/gayle.png';
+import gene from './images/gene.png';
+import jimmyjr from './images/jimmyjr.png';
+import linda from './images/linda.png';
+import louise from './images/louise.png';
+import teddy from './images/teddy.png';
+import tina from './images/tina.png';
+
+const images = [
+  bob,
+  gayle,
+  gene,
+  jimmyjr,
+  linda,
+  louise,
+  teddy,
+  tina
+];
+
+export default images;
